85004f82c5e74246fa6830669d42d393b299ae10
Alpheratzy <alexandraronvera@gmail.com>
2023-09-06 12:13:40 -0300

    css base, dos pantallas de formulario de inscripcion,  intento de ruteo

---
+++ src/components/home.js
@@ -0,0 +1,9 @@
+function home() {
+  const homeAlone = document.createElement('section');
+
+  homeAlone.innerHTML = '<h2>Aqui va el login</h2>';
+
+  return homeAlone;
+}
+
+export default home;
--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
-// Este es el punto de entrada de tu aplicacion
+import home from './components/home.js';
+import newUser from './components/NewUserForm.js';
+// eslint-disable-next-line import/no-named-as-default
+import preferences from './components/preferences.js';
 
-import { myFunction } from './lib/index.js';
+const root = document.getElementById('root');
 
-myFunction();
+const routes = [
+  { path: '/', component: home },
+  { path: '/newUser', component: newUser },
+  { path: '/preferences', component: preferences },
+];
+
+const defaultRoute = '/';
+
+function navigateTo(hash) {
+  const route = routes.find((routeFind) => routeFind.path === hash);
+
+  if (route && route.component) {
+    window.history.pushState(
+      {},
+      route.path,
+      window.location.origin + route.path,
+    );
+    root.appendChild(route.component());
+  }
+}
+navigateTo('/newUser');
+// root.append(preferences());

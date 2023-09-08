2669404be9d28974f8f2e89a085f5e2fd8db810a
Ketzalikab <blancoka878@gmail.com>
2023-09-08 15:14:10 -0600

    commit previo para traer los docs de Sahara

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,13 +1,33 @@
+// Import the functions you need from the SDKs you need
+
 import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 
 const firebaseConfig = {
+
   apiKey: 'AIzaSyCyFPrq4c7FtYSntpkFqOCXeE1Bl03SzEA',
+
   authDomain: 'colective-iscool.firebaseapp.com',
+
   projectId: 'colective-iscool',
+
   storageBucket: 'colective-iscool.appspot.com',
+
   messagingSenderId: '423977582808',
+
   appId: '1:423977582808:web:4a2df0105bb6502d35d7ad',
+
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
+// Initialize Firebase
+// En esta variable esta guardada la app de Firebase.
+export const firebaseApp = initializeApp(firebaseConfig);
 export default firebaseApp;
+// En esta variable le instalo el auth a la app.
+export const auth = getAuth(firebaseApp);

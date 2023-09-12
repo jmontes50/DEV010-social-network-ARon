540fa7765791d8947c8e59a4d423cc01f7fcbea5
Alpheratzy <alexandraronvera@gmail.com>
2023-09-12 14:10:10 -0300

    le arreglé la navegación que se había poteado

--- src/components/NewUserForm.js
+++ src/components/NewUserForm.js
@@ -1,7 +1,7 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { auth } from './firebase.js';
 
-function newUser() {
+function newUser(navigateTo) {
   const newUserForm = document.createElement('section');
 
   newUserForm.innerHTML = `<img id="logo" src= "img/logo.png">
@@ -77,6 +77,7 @@ function newUser() {
       });
 
     registerForm.reset();
+    window.location.href = '/preferences';
   });
 
   return newUserForm;

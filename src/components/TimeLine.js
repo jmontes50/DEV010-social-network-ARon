d40a12368325a8d809bb3ee3d5301fe9c4503794
Sahara <sahararod@gmail.com>
2023-10-03 10:28:06 -0600

    se mejoro funcion de editar, edita correctamente

--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -45,11 +45,6 @@ function TimeLine() {
   const commentList = document.createElement('ul');
   commentList.setAttribute('id', 'commentList');
 
-  const isEditing = false;
-  const isLiking = false;
-  const likes = 0;
-  let nameLike;
-
   const userContainer = document.createElement('div');
   userContainer.setAttribute('class', 'user-container');
 
@@ -75,13 +70,13 @@ function TimeLine() {
     console.log('No se encontró un nombre de usuario en el localStorage.');
   }
 
-  const btnLike = document.createElement('img');
+  /* const btnLike = document.createElement('img');
   btnLike.setAttribute('id', 'btnLike');
   btnLike.setAttribute('src', './assets/unlike.png');
 
   const sumLikes = document.createElement('span');
   sumLikes.setAttribute('id', 'sumLikes');
-  sumLikes.innerHTML = likes;
+  sumLikes.innerHTML = likes; */
 
   // cliks
   /* sendButton.addEventListener('click', () => {
@@ -182,6 +177,7 @@ function TimeLine() {
   sendButton.addEventListener('click', () => {
     const commentText = commentInput.value;
     savePost(selectedUserName, selectedImage, commentText);
+    const likes = 0;
     const postLi = postCreate(selectedImage, selectedUserName, likes, commentText);
     commentList.appendChild(postLi);
     // mandar post a DB (userID, icon, idLikes, post, time)

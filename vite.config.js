dc36f5ad0be80c84f72600ea434b3013df27760f
Jorge Montesinos <osmarmontes50@gmail.com>
2023-10-17 12:08:16 -0500

    update base

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig(({ command }) => {
       // para o nome/url do seu repositório
       // https://vitejs.dev/guide/static-deploy.html#github-pages
       // ejemplo base: '/social-network/',
-      base: '/jmontes50/DEV010-social-network-ARon/',
+      base: '/',
       build: {
         minify: false,
         rollupOptions: {
